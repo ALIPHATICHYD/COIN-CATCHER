08361cbab483534dba349a6926bd5c2296d87140
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-09-03 11:55:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = 250;
+    var y = 150;
+    var coinx = Math.random() * (600-50);
+	var coiny = Math.random() * (400-50);
